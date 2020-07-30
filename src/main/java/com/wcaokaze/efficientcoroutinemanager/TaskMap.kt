68bb540f1a1051887cfb46f5429177708fef5d06
wcaokaze <wcaokaze@gmail.com>
2020-07-31 00:52:10 +0900

    added TaskMap

---
+++ src/main/java/com/wcaokaze/efficientcoroutinemanager/TaskMap.kt
@@ -0,0 +1,57 @@
+/*
+ * Copyright 2020 wcaokaze
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.wcaokaze.efficientcoroutinemanager
+
+import kotlinx.coroutines.*
+import kotlin.coroutines.*
+
+fun CoroutineScope.launch(
+      context: CoroutineContext = EmptyCoroutineContext,
+      start: CoroutineStart = CoroutineStart.DEFAULT,
+      taskId: Any,
+      block: suspend CoroutineScope.() -> Unit
+): Job {
+   val taskMap = context[TaskMap]!!
+
+   synchronized (taskMap) {
+      var job = taskMap[taskId]
+      if (job != null) { return job }
+
+      job = launch(context, start, block)
+      taskMap[taskId] = job
+      return job
+   }
+}
+
+@Suppress("FunctionName")
+fun TaskMap(): TaskMap = TaskMapImpl()
+
+interface TaskMap : CoroutineContext.Element {
+   companion object Key : CoroutineContext.Key<TaskMap>
+
+   operator fun get(taskId: Any): Job?
+   operator fun set(taskId: Any, job: Job)
+}
+
+private class TaskMapImpl : TaskMap {
+   private val jobMap = HashMap<Any, Job>()
+
+   override val key get() = TaskMap
+
+   override fun get(taskId: Any): Job? = jobMap[taskId]
+   override fun set(taskId: Any, job: Job) { jobMap[taskId] = job }
+}
---
+++ src/test/java/com/wcaokaze/efficientcoroutinemanager/TaskMapTest.kt
@@ -0,0 +1,53 @@
+/*
+ * Copyright 2020 wcaokaze
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.wcaokaze.efficientcoroutinemanager
+
+import org.junit.runner.*
+import org.junit.runners.*
+import kotlin.test.*
+
+import kotlinx.coroutines.*
+import java.util.*
+
+@RunWith(JUnit4::class)
+class TaskMapTest {
+   @Test fun 重複なし_すべて実行() {
+      val results = LinkedList<Int>()
+
+      runBlocking {
+         val taskMap = TaskMap()
+
+         launch(taskMap, taskId = 0) { results += 0 }
+         launch(taskMap, taskId = 1) { results += 1 }
+      }
+
+      assertEquals(listOf(0, 1), results)
+   }
+
+   @Test fun 重複あり_先にlaunchした方のみ実行() {
+      val results = LinkedList<Int>()
+
+      runBlocking {
+         val taskMap = TaskMap()
+
+         launch(taskMap, taskId = 0) { results += 0 }
+         launch(taskMap, taskId = 0) { results += 1 }
+      }
+
+      assertEquals(listOf(0), results)
+   }
+}
